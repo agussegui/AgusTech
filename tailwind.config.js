a60103b18d4eff6fe8624bfd0d934ac91e8e7599
AgustinSegui <agussegui24@hotmail.es>
2024-12-05 19:33:59 -0300

    feat: change tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    "./src/**/*.{jsx,tsx,js,ts}",
+    "./index.html",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
